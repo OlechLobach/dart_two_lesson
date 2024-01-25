859cf24ce7c055a6f16cb1295973276377ac72d6
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-01-25 19:16:24 +0200

    first commit

---
+++ bin/dart_two_lesson.dart
@@ -0,0 +1,5 @@
+import 'package:dart_two_lesson/dart_two_lesson.dart' as dart_two_lesson;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_two_lesson.calculate()}!');
+}
---
+++ test/dart_two_lesson_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_two_lesson/dart_two_lesson.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
